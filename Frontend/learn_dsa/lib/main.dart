1ade868ba68b118edb0fb9ed23be3a24c38155b3
Bea <99785877+ErsekBeatriceAdrienne@users.noreply.github.com>
2024-10-25 08:28:38 +0300

    new folder structure

---
+++ Frontend/learn_dsa/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:learn_dsa/pages/login_page.dart';
+
+void main()
+{
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget
+{
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>
+{
+  ThemeMode _themeMode = ThemeMode.light;
+
+  void _toggleTheme()
+  {
+    setState(()
+    {
+      _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      title: 'Learn DSA',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: _themeMode,
+      home: LoginPage(toggleTheme: _toggleTheme),
+    );
+  }
+}
